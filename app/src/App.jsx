938a879d7532441341b2786e2f5d9678467d52d5
Andrew Hunt <andrew.hunt@musios.app>
2025-02-15 09:47:37 +1100

    recover branch

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import musiosLogo from './assets/images/musios-logo-black-circle.svg'
 // import projectLogo from './assets/images/spotify-playlist-to-gigperformer-icon.svg'
 import PlaylistForm from './PlaylistForm'
 import PlaylistSummary from './PlaylistSummary'
@@ -16,31 +15,8 @@ function App() {
   return (
     <>
       <div className="container align-items-start text-center w-100">
-        <div className="" style={{border: "1px solid #404050", borderRadius: "12px", backgroundColor: "#e0e0e0", padding: "16px 24px", marginBottom: "32px", }}>
-
-          <div className="row">
-            <h1 className="mb-0">
-              <img src={musiosLogo} alt="Musios Logo" className="logo" style={{display: "inline", maxHeight: "80px", paddingRight: "30px"}}/>
-              Spotify Playlist to Gig Performer .gig file
-            </h1>
-          </div>
-
-
-          <span style={{margin: "0 10px"}}>
-            by <a href="https://musios.app">Musios.app</a>
-          </span>
-          | 
-          <span style={{margin: "0 10px"}}>
-            <a href="https://musios.app/projects/spotify-playlist-to-gigperformer/README.html"> Documentation </a>
-          </span>
-          | 
-          <span style={{margin: "0 10px"}}>
-            <a href="https://github.com/musios-app/spotify-playlist-to-gigperformer"> GitHub </a>
-          </span>
-          | 
-          <span style={{margin: "0 10px"}}>
-            <a href="https://github.com/orgs/musios-app/discussions"> Comments & Support </a>
-          </span>
+        <div className="row">
+          <h1 className="mb-0">Spotify Playlist to Gig Performer .gig file</h1>
         </div>
 
         <div className="row my-4" style={{display: "none"}}>
