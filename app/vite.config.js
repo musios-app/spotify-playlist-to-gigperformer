dbf8a63659fde6c6e7894d93b2e85d0a2fac1ba1
Andrew Hunt <andrew.hunt@musios.app>
2025-02-15 10:02:18 +1100

    reinstate work

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist'
+    outDir: '../tool'
   },
-  base: '/projects/spotify-playlist-to-gigperformer/dist'
+  base: '/projects/spotify-playlist-to-gigperformer/tool'
 })
